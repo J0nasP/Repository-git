996334187625d39226ccbf906531d88c6acc086f
J0nasP <petersen0108@gmail.com>
2021-03-01 11:26:01 +0100

    Gange og plus addede

--- NewStuff/NewStuff/Program.cs
+++ NewStuff/NewStuff/Program.cs
@@ -8,5 +8,21 @@ namespace NewStuff
         {
             Console.WriteLine("Hello World!");
         }
+
+
+        static int plus(int tal1, int tal2)
+        {
+            int resultat = tal1 + tal2;
+
+            return resultat;
+        }
+
+        static int gange(int tal1, int tal2)
+        {
+            int resultat = tal1 * tal2;
+
+            return resultat;
+        }
     }
+
 }
