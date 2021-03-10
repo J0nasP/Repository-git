75e0c5d100757533becd89c4ab3cd1886e2bbfe3
J0nasP <petersen0108@gmail.com>
2021-03-10 11:07:57 +0100

    Added comments to math functions

--- NewStuff/NewStuff/Program.cs
+++ NewStuff/NewStuff/Program.cs
@@ -2,24 +2,41 @@
 
 namespace NewStuff
 {
+    /// <summary>
+    /// This program is a simple start to a calculator with different math functions
+    /// </summary>
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lommeregner");
         }
-
-
+        // adds two numbers together
         static int plus(int tal1, int tal2)
         {
             int resultat = tal1 + tal2;
 
             return resultat;
         }
+        // subtract two numbers 
+        static int minus(int tal1, int tal2)
+        {
+            int resultat = tal1 + tal2;
 
+            return resultat;
+        }
+        // Multipli two numbers togéther
         static int gange(int tal1, int tal2)
         {
             int resultat = tal1 * tal2;
+
+            return resultat;
+        }
+        // Divide two numbers
+        static int dividere(int tal1, int tal2)
+        {
+            int resultat = tal1 / tal2;
 
             return resultat;
         }
